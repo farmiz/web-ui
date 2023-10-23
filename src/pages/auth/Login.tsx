e3dccae7be1b23bf95a39531aef551d802b385c0
Bernard Arhia <bernardarhia@gmail.com>
2023-10-23 19:30:44 +0000

    Update Login.tsx

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -56,7 +56,6 @@ export default function AuthenticationPage() {
 
   return (
     <>
-    {process.env.NODE_ENV}
       <div className="container relative h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
